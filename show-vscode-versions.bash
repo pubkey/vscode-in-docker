e9e4dbfc143f088bc1921fc3e1f01b3b9fd8abbd
pubkey <8926560+pubkey@users.noreply.github.com>
2021-06-23 08:41:01 +0200

    ADD show available vscode versions in CI

---
+++ show-vscode-versions.bash
@@ -0,0 +1,10 @@
+apt-get update
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg \
+  && mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg \
+  && sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+apt-get update && apt-get install -y code
+
+# show available vscode versions
+apt-cache policy code
